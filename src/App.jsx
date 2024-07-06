bd000784787039b98223f3d7d8e4978dd92afdb2
Harsh Dadhich <121938001+theharshdadhich@users.noreply.github.com>
2024-07-06 22:11:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,24 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+// import React from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { useContext } from 'react'
+import { PlayerContext } from './context/PlayerContext'
 
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
- 
 const App = () => {
-  const { activeSong } = useSelector((state) => state.player);
+
+  const {audioRef, track} = useContext(PlayerContext);
 
   return (
-    <div className="relative flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <Searchbar />
-
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-          <div className="flex-1 h-fit pb-40">
-            <Routes>
-              <Route path="/" element={<Discover />} />
-              <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
-              <Route path="/around-you" element={<AroundYou />} />
-              <Route path="/artists/:id" element={<ArtistDetails />} />
-              <Route path="/songs/:songid" element={<SongDetails />} />
-              <Route path="/search/:searchTerm" element={<Search />} />
-            </Routes>
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
-          </div>
-        </div>
+    <div className='h-screen bg-black'>
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display />
       </div>
-
-      {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-          <MusicPlayer />
-        </div>
-      )}
+      <Player />
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
